b064dc717198ba5a7ccfaaa2fd2e94164d063877
Paul <contact@trust-gaming.fr>
2024-05-10 14:25:36 +0200

    Initial Commit

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    new Typewriter('#infosAnim', {
+        strings: ['le ciseaux...', 'la pierre...', 'le papier...'],
+        autoStart: true,
+        loop: true
+    });
+});
